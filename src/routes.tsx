39196a25df485ab2bddfddc04dd8db771a2f0cf8
Eduardo Silva <edusilvatele@gmail.com>
2023-09-30 10:23:04 -0300

    Configurando rotas padrão

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import MainLayout from "./layouts/MainLayout";
 
 export default function Router() {
     return (
@@ -10,7 +11,18 @@ export default function Router() {
         de acordo com o path do navegador. */}
         {/* Ja o Route, é o arquivo que especifica a rota, e o elemento que será carregado */}
         <Routes>
-            <Route path="/" element={<Home />} />
+            {/* Configurando a pagina mãe:
+                1. Vai receber o path absoluto da página. 
+                2. Carrega o elemento pai (que deve conter o <Outlet> para renderização dos filhos)
+                3. Dentro da rota, são passadas as rotas filhas;
+                4. A rota index, vai ser a rota a ser renderizada quando bater no path da mãe;
+                5. As demais rotas filhas devem conter o path relativo. ex: path: 'cadastro', pois se 
+                for passado o path: '/cadastro', o router vai entender que é uma rota absoluta e não uma
+                rota filha.
+            */}
+            <Route path="/" element={<MainLayout />}>
+                <Route index element={<Home />} />
+            </Route>
         </Routes>
         </BrowserRouter>
     )
